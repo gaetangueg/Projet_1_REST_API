d10dfc3d99e7a2b7323981a994f0f17fb1577d54
gaetan <gaetan.guegan951@gmail.com>
2024-11-08 15:36:08 +0100

    observation_users_à_partir_base_de_donnee

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const router = express.Router()
 
-const usersArray = [
+/*const usersArray = [
     { id: 1, firstName: "John", lastName: "Doe", role: "admin" },
     { id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
     { id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
     { id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
     { id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
-];
+];*/
+
+const db = require("../database")
 
 // Endpoint pour obtenir tous les utilisateurs
 router.get("/users", (req, res) => {
-    res.json(usersArray);
+	db.all('SELECT * FROM users', [], (err, rows) => {
+		if (err) {
+		  res.status(500).json({ error: err.message });
+		} else {
+		  res.json(rows);
+		}
+	  });
+	
 });
 
 
